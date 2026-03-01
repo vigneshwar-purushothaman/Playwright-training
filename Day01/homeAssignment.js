fe70f31f86b96c1e610da90834f3a870a87ce248
vigneshwar-purushothaman <vigneshwar123@gmail.com>
2026-03-01 14:00:12 +0530

    Add files via upload
    Day1 Assignments

---
+++ Day01/homeAssignment.js
@@ -0,0 +1,19 @@
+const { type } = require("node:os")
+
+let userName = `Vignesh`
+let orgName = `Cognizant`
+let contactNo = 9843430990
+let knowsAutomation = true
+let usesPlaywright
+
+console.log("My name is " +userName)
+console.log("Data type of username is " +typeof userName)
+
+console.log("My organisation name is " +orgName)
+console.log(contactNo)
+console.log(typeof contactNo)
+console.log(knowsAutomation)
+console.log(typeof knowsAutomation)
+console.log(usesPlaywright)
+console.log(typeof usesPlaywright)
+
---
+++ Day01/primitiveDatatypes.js
@@ -0,0 +1,18 @@
+var firstName = "Vignesh"
+var companyName = "Cognizant"
+var mobileNumber = 9967876889
+var isAutomation = true
+var hasPlaywright
+var hasValue = null
+
+console.log("Name : " +firstName)
+console.log(typeof firstName)
+console.log("Company Name : " +companyName)
+console.log(mobileNumber)
+console.log(typeof mobileNumber)
+console.log(isAutomation)
+console.log(typeof isAutomation)
+console.log(typeof hasPlaywright)
+console.log(typeof hasValue)
+
+
